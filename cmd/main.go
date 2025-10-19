0c1a375a1daaff757bf9c451811bd461276dde02
Md. Sadiq <mohammadsadiq4950@gmail.com>
2025-10-19 09:08:07 +0530

    Image generation done

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	w, h := 500, 150
+	r := image.Rect(0, 0, w, h)
+	img := image.NewRGBA(r)
+
+	for y := range h {
+		for x := range w {
+			img.Set(x, y, color.RGBA{
+				R: uint8((x + y) & 255),
+				G: uint8((x + y) << 1 & 255),
+				B: uint8((x + y) << 1 & 255),
+				A: 255,
+			})
+		}
+	}
+
+	f, err := os.Create("image.png")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	err = png.Encode(f, img)
+	if err != nil {
+		panic(err)
+	}
 }
