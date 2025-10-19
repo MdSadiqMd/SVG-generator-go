e8f9d696c140b540fac5cb75fc25d2c488b64d5a
Md. Sadiq <mohammadsadiq4950@gmail.com>
2025-10-19 14:43:06 +0530

    It works for now and svgo is awesome

--- cmd/main.go
+++ cmd/main.go
@@ -1,73 +1,37 @@
 package main
 
 import (
-	"image"
-	"image/color"
-	"image/draw"
-	"image/png"
+	"log"
+	"math/rand"
 	"os"
+	"time"
+
+	svg "github.com/ajstarks/svgo"
 )
 
+func rn(n int) int {
+	return rand.Intn(n)
+}
+
 func main() {
-	data := []int{100, 33, 73, 64}
-	w, h := len(data)*60+10, 100
-
-	r := image.Rect(0, 0, w, h)
-	img := image.NewRGBA(r)
-	bg := image.NewUniform(color.RGBA{240, 240, 240, 255})
-	draw.Draw(img, img.Bounds(), bg, image.Point{0, 0}, draw.Src)
-
-	mask := image.NewRGBA(image.Rect(0, 0, w, h))
-	for y := range h {
-		for x := range w {
-			alpha := 0
-			switch {
-			case y >= 0 && y < 30:
-				alpha = 255
-			case y >= 30 && y < 60:
-				alpha = 100
-			}
-			mask.Set(x, y, color.RGBA{
-				R: uint8((x + y) & 255),
-				G: uint8((x + y) << 1 & 255),
-				B: uint8((x + y) << 1 & 255),
-				A: uint8(alpha),
-			})
-		}
-	}
-
-	// for y := range h {
-	// 	for x := range w {
-	// 		img.Set(x, y, color.RGBA{255, 255, 255, 255})
-	// 	}
-	// }
-
-	for i, dp := range data {
-		x0, y0 := (i*60 + 10), 100-dp
-		x1, y1 := (i+1)*60-1, 100
-		bar := image.Rect(x0, y0, x1, y1)
-
-		grey := image.NewUniform(color.RGBA{180, 180, 180, 255})
-		draw.Draw(img, bar, grey, image.Point{0, 0}, draw.Src)
-
-		red := image.NewUniform(color.RGBA{250, 180, 180, 255})
-		draw.DrawMask(img, bar, red, image.Point{0, 0}, mask, image.Point{x0, y0}, draw.Over)
-
-		// for x := i*60 + 10; x < i*60+60; x++ {
-		// 	for y := 100; y >= (100 - dp); y-- {
-		// 		img.Set(x, y, color.RGBA{180, 180, 250, 255})
-		// 	}
-		// }
-	}
-
-	f, err := os.Create("image.png")
+	f, err := os.OpenFile("output.svg", os.O_CREATE|os.O_WRONLY, 0755)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to create file: %v", err)
 	}
 	defer f.Close()
 
-	err = png.Encode(f, img)
-	if err != nil {
-		panic(err)
+	//data:=[]int{100,33,73,64}
+	canvas := svg.New(f)
+	width, height, nstars := 500, 500, 250
+	style := "font-size:48pt;fill:white;text-anchor:middle"
+
+	rand.Seed(time.Now().Unix())
+	canvas.Start(width, height)
+	canvas.Rect(0, 0, width, height)
+	for range nstars {
+		canvas.Circle(rn(width), rn(height), rn(3), "fill:white")
 	}
+	canvas.Circle(width/2, height/2, 100, "fill:red")
+	canvas.Text(width/2, height/2, "Hello World", style)
+	canvas.End()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MdSadiqMd/svg-generator-go
 
 go 1.24.9
+
+require github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
